c535b91250f34b21f59dd8ddb52dd349c8d1bb57
diazgonza17 <diazgonzalo1720@gmail.com>
2023-04-14 01:27:03 -0300

    Added edit and getProfile methods, renamed request paths

--- portfolioBack/src/main/java/com/yoprogramo/portfolioBack/controller/Controller.java
+++ portfolioBack/src/main/java/com/yoprogramo/portfolioBack/controller/Controller.java
@@ -6,33 +6,56 @@ import com.yoprogramo.portfolioBack.service.IPersonaService;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin (origins="http://localhost:4200")
 public class Controller {
     
     @Autowired
     private IPersonaService persoServ;
     
-    @PostMapping ("/new/persona")
+    @PostMapping ("/persona/new")
     public void agregarPersona(@RequestBody Persona pers) {
         persoServ.crearPersona(pers);
     }
     
-    @GetMapping ("/ver/personas")
+    @GetMapping ("/persona/all")
     @ResponseBody
     public List<Persona> verPersonas() {
         return persoServ.verPersonas();
     }
     
-    @DeleteMapping ("/delete/{id}")
+    @DeleteMapping ("/persona/delete/{id}")
     public void borrarPersona (@PathVariable Long id){
         persoServ.borrarPersona(id);
     }
+    
+    @PutMapping("/persona/edit/{id}")
+    public Persona editPersona(@PathVariable Long id,
+                               @RequestParam("nombre") String nuevoNombre,
+                               @RequestParam("apellido") String nuevoApellido) {
+        Persona perso = persoServ.buscarPersona(id);
+        
+        perso.setNombre(nuevoNombre);
+        perso.setApellido(nuevoApellido);
+    
+        persoServ.crearPersona(perso);
+        return perso;
+    }
+    
+    @GetMapping("persona/perfil")
+    public Persona buscarPersona() {
+        return persoServ.buscarPersona(1L);
+    }
+
 }
