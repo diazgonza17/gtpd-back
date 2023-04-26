f285b1af51e743c2998d36879eddbff4ff6b5e69
diazgonza17 <diazgonzalo1720@gmail.com>
2023-04-25 23:51:35 -0300

    Deleted innecesary methods in Persona

--- portfolioBack/src/main/java/com/yoprogramo/portfolioBack/controller/CPersona.java
+++ portfolioBack/src/main/java/com/yoprogramo/portfolioBack/controller/CPersona.java
@@ -4,16 +4,11 @@ package com.yoprogramo.portfolioBack.controller;
 import com.yoprogramo.portfolioBack.dto.dtoPersona;
 import com.yoprogramo.portfolioBack.model.Persona;
 import com.yoprogramo.portfolioBack.security.controller.Mensaje;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.yoprogramo.portfolioBack.service.SPersona;
 import org.apache.commons.lang3.StringUtils;
@@ -28,50 +23,27 @@ public class CPersona {
     
     @Autowired
     SPersona sPersona;
-    
-    @GetMapping ("/all")
-    @ResponseBody
-    public ResponseEntity<List<Persona>> all() {
-        return new ResponseEntity(sPersona.getAll(), HttpStatus.OK);
-    }
-    
-    @GetMapping("/detail/{id}")
-    public ResponseEntity<Persona> detail(@PathVariable("id") int id) {
-        if(!sPersona.existsById(id)){
+
+    @GetMapping("/detail")
+    public ResponseEntity<Persona> detail() {
+        if(!sPersona.existsById(1)){
             return new ResponseEntity(new Mensaje("La persona no existe"), HttpStatus.NOT_FOUND);
         }
         
-        Persona persona = sPersona.getById(id).get();
+        Persona persona = sPersona.getById(1).get();
         return new ResponseEntity(persona, HttpStatus.OK);
     }
     
-    @PostMapping ("/create")
-    public ResponseEntity<?> create(@RequestBody dtoPersona dtoPerso) {
-        if(StringUtils.isBlank(dtoPerso.getNombre())){
-            return new ResponseEntity(new Mensaje("El nombre es obligatorio"), HttpStatus.BAD_REQUEST);
-        }
-        if(sPersona.existsByNombre(dtoPerso.getNombre())){
-            return new ResponseEntity(new Mensaje("La persona ya existe"), HttpStatus.BAD_REQUEST);
-        }
-        
-        Persona persona = new Persona(dtoPerso.getNombre(), dtoPerso.getApellido(), dtoPerso.getTitulo(), dtoPerso.getUbicacion(), dtoPerso.getAbout());
-        sPersona.save(persona);
-        return new ResponseEntity(new Mensaje("Persona agregada"), HttpStatus.OK);
-    }
-    
-    @PutMapping("/update/{id}")
-    public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody dtoPersona dtoPerso) {
-        if(!sPersona.existsById(id)){
+    @PutMapping("/update")
+    public ResponseEntity<?> update(@RequestBody dtoPersona dtoPerso) {
+        if(!sPersona.existsById(1)){
             return new ResponseEntity(new Mensaje("El ID no existe"), HttpStatus.NOT_FOUND);
         }
-        if(sPersona.existsByNombre(dtoPerso.getNombre()) && sPersona.getByNombre(dtoPerso.getNombre()).get().getId() != id){
-            return new ResponseEntity(new Mensaje("La persona ya existe"), HttpStatus.BAD_REQUEST);
-        }
         if(StringUtils.isBlank(dtoPerso.getNombre())){
             return new ResponseEntity(new Mensaje("El nombre es obligatorio"), HttpStatus.BAD_REQUEST);
         }
         
-        Persona persona = sPersona.getById(id).get();
+        Persona persona = sPersona.getById(1).get();
         persona.setNombre(dtoPerso.getNombre());
         persona.setApellido(dtoPerso.getApellido());
         persona.setTitulo(dtoPerso.getTitulo());
@@ -79,15 +51,5 @@ public class CPersona {
         persona.setAbout(dtoPerso.getAbout());
         sPersona.save(persona);
         return new ResponseEntity(new Mensaje("Persona actualizada"), HttpStatus.OK);
-    }
-    
-    @DeleteMapping ("/delete/{id}")
-    public ResponseEntity<?> delete(@PathVariable("id") int id){
-        if(!sPersona.existsById(id)){
-            return new ResponseEntity(new Mensaje("El ID no existe"), HttpStatus.NOT_FOUND);
-        }
-        
-        sPersona.deleteById(id);
-        return new ResponseEntity(new Mensaje("Persona eliminada"), HttpStatus.OK);
     }
 }
--- portfolioBack/src/main/java/com/yoprogramo/portfolioBack/service/SPersona.java
+++ portfolioBack/src/main/java/com/yoprogramo/portfolioBack/service/SPersona.java
@@ -2,7 +2,6 @@
 package com.yoprogramo.portfolioBack.service;
 
 import com.yoprogramo.portfolioBack.model.Persona;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.yoprogramo.portfolioBack.repository.IRPersona;
@@ -13,32 +12,16 @@ public class SPersona {
 
     @Autowired
     IRPersona rPersona;
-    
-    public List<Persona> getAll() {
-        return rPersona.findAll();
-    }
-    
+
     public Optional<Persona> getById(int id) {
         return rPersona.findById(id);
-    }
-    
-    public Optional<Persona> getByNombre(String nombre){
-        return rPersona.findByNombre(nombre);
     }
 
     public void save(Persona per) {
         rPersona.save(per);
     }
-
-    public void deleteById(int id) {
-        rPersona.deleteById(id);
-    }
     
     public boolean existsById(int id){
         return rPersona.existsById(id);
-    }
-    
-    public boolean existsByNombre(String nombre){
-        return rPersona.existsByNombre(nombre);
     }
 }
