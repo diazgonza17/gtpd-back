f574e827bbdc331d867490a473ee04ddad3500e5
diazgonza17 <diazgonzalo1720@gmail.com>
2023-04-30 16:58:13 -0300

    Modified Auth CORS

--- portfolioBack/src/main/java/com/yoprogramo/portfolioBack/security/controller/AuthController.java
+++ portfolioBack/src/main/java/com/yoprogramo/portfolioBack/security/controller/AuthController.java
@@ -31,7 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins={"http://localhost:4200"})
+@CrossOrigin(origins={"https://portfolio-back-kojp.onrender.com"})
 public class AuthController {
     
     @Autowired
